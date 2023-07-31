f4eb8fe6858c71dbb4002569fb85aa0d52325cee
Bethan Cracknell Daniels <66028934+bnc19@users.noreply.github.com>
2023-07-31 14:21:04 +0100

    add description of code

--- R/funct_save_cluster_output.R
+++ R/funct_save_cluster_output.R
@@ -8,10 +8,16 @@ save_cluster_output = function(task_bundle_name, output_path = "output") {
   
   # extract results from each task bundle 
   all_tasks  = obj$task_bundle_get(task_bundle_name)
-  results = all_tasks$results()
+
+  results = all_tasks$results() 
  
+  
+# The function run_model returns a single data.frame, the object results is a 
+# list of 100 data.frames. Therefore we can easily merge the results from all
+# 100 jobs to one data.frame using the bind_rows function: 
+
   summarise_results  =  
-    results %>%  bind_rows() %>%  # merge the results from all 100 jobs to one data.frame 
+    results %>%  bind_rows() %>% 
     group_by(split, metric) %>% 
     summarise(mean = mean(value), # calclate mean and 95% C()
               lower = quantile(value, p = 0.025),
@@ -21,7 +27,7 @@ save_cluster_output = function(task_bundle_name, output_path = "output") {
   # save files 
   
   write.csv(summarise_results, 
-            file = paste0(output_path, "summarise_results.csv"))  
+            file = paste0(output_path, "/", task_bundle_name, "_results.csv"))  
   
   
   return(summarise_results) } 
